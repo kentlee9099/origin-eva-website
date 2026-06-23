079464268dfb2d0752a6998a4dc923cf1e8dacc5
kentlee9099 <kentlee9099@gmail.com>
2026-06-24 02:40:06 +0800

    Delete src directory

--- src/i18n/translations.ts
+++
@@ -1,5 +0,0 @@
-import data from './data.json';
-
-export type LangCode = 'cn' | 'en' | 'ru';
-export type Translations = typeof data.cn;
-export const translations = data as Record<LangCode, Translations>;
